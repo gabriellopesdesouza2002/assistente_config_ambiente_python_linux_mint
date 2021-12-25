3e9f6942a0f264d6a7528f90dedda271028ec4e1
Gabriel <65515076+gabriellopesdesouza2002@users.noreply.github.com>
2021-12-25 13:32:17 -0300

    fix: Arrumando e refatorando o código.

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 echo 'Esse é o assistente de configuração de ambiente para desenvolvimento em Python'
 
-sleep 4  # Espera 4 segundos
+sleep 2  # Espera 2 segundos
 
 echo ''
 
@@ -13,28 +13,27 @@ sleep 2  # Espera 2 segundos
 echo '' 
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl git -y
-sudo apt install python3 python3-dev python3-venv idle-python3.8 python3-pip virtualenv gcc default-libmysqlclient-dev libssl-dev -y
+sudo apt install curl git python3 python3-dev python3-venv idle-python3.8 python3-pip virtualenv gcc default-libmysqlclient-dev libssl-dev -y
 
-sleep 3  # Espera 3 segundos
+sleep 2  # Espera 2 segundos
 
 clear
 
 sleep 2  # Espera 2 segundos
 
-echo "Você quer instalar o Snap? (y) || (n)"
+echo "Você quer instalar o Snap? [y|n]"
 
 read installsnap
 
 if [ $installsnap == 'y' ]; then
 	sudo rm /etc/apt/preferences.d/nosnap.pref
 	sudo apt update
-	sudo apt install snapd
+	sudo apt install snapd -y
 fi
 
 
 echo "Instalando o Pycharm Community"
 
-sleep 3
+sleep 2  # Espera 2 segundos
 
-sudo snap install pycharm-community --classic
+sudo snap install pycharm-community --classic -y
